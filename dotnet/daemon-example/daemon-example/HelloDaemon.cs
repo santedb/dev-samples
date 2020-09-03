7c7d075d5a3aac17201ba5dac71aeb5edf1e31fb
Justin Fyfe <justin@fyfesoftware.ca>
2020-09-02 20:10:05 -0400

    Minor fixes to the daemon example

--- dotnet/daemon-example/daemon-example/HelloDaemon.cs
+++ dotnet/daemon-example/daemon-example/HelloDaemon.cs
@@ -24,76 +24,76 @@ using System.Threading;
 
 namespace daemon_example
 {
+/// <summary>
+/// The HelloDaemon is a simple Daemon that prints "HELLO WORLD" on the trace log
+/// </summary>
+public class HelloDaemon : IDaemonService
+{
+
+    // Tracer
+    private Tracer m_tracer = Tracer.GetTracer(typeof(HelloDaemon));
+
+    // A simple timer 
+    private Timer m_helloTimer;
+
     /// <summary>
-    /// The HelloDaemon is a simple Daemon that prints "HELLO WORLD" on the trace log
+    /// True when running
     /// </summary>
-    public class HelloDaemon : IDaemonService
+    public bool IsRunning => this.m_helloTimer != null;
+
+    /// <summary>
+    /// Service name
+    /// </summary>
+    public string ServiceName => "HelloDaemon";
+
+    /// <summary>
+    /// Fired when the daemon is starting
+    /// </summary>
+    /// <remarks>This event is sometimes used by other services to get notified when your service starts (see the chaining example)</remarks>
+    public event EventHandler Starting;
+    /// <summary>
+    /// Fired after this daemon has started
+    /// </summary>
+    public event EventHandler Started;
+    /// <summary>
+    /// Fired when the service is stopping
+    /// </summary>
+    public event EventHandler Stopping;
+    /// <summary>
+    /// Fired when the service has stopped
+    /// </summary>
+    public event EventHandler Stopped;
+
+    /// <summary>
+    /// Start the hello daemon
+    /// </summary>
+    public bool Start()
     {
+        // Notify other modules we're starting
+        this.Starting?.Invoke(this, EventArgs.Empty);
 
-        // Tracer
-        private Tracer m_tracer = Tracer.GetTracer(typeof(HelloDaemon));
+        // Fire up the timer
+        this.m_helloTimer = new Timer((_) => this.m_tracer.TraceInfo("Hello World!"), null, 1000, 1000);
 
-        // A simple timer 
-        private Timer m_helloTimer;
-
-        /// <summary>
-        /// True when running
-        /// </summary>
-        public bool IsRunning => this.m_helloTimer != null;
-
-        /// <summary>
-        /// Service name
-        /// </summary>
-        public string ServiceName => "HelloDaemon";
-
-        /// <summary>
-        /// Fired when the daemon is starting
-        /// </summary>
-        /// <remarks>This event is sometimes used by other services to get notified when your service starts (see the chaining example)</remarks>
-        public event EventHandler Starting;
-        /// <summary>
-        /// Fired after this daemon has started
-        /// </summary>
-        public event EventHandler Started;
-        /// <summary>
-        /// Fired when the service is stopping
-        /// </summary>
-        public event EventHandler Stopping;
-        /// <summary>
-        /// Fired when the service has stopped
-        /// </summary>
-        public event EventHandler Stopped;
-
-        /// <summary>
-        /// Start the hello daemon
-        /// </summary>
-        public bool Start()
-        {
-            // Notify other modules we're starting
-            this.Starting?.Invoke(this, EventArgs.Empty);
-
-            // Fire up the timer
-            this.m_helloTimer = new Timer((_) => this.m_tracer.TraceInfo("Hello World!"), null, 1000, 1000);
-
-            // Notify of start
-            this.Started?.Invoke(this, EventArgs.Empty);
-            return true;
-        }
-
-        /// <summary>
-        /// Stop the hello daemon
-        /// </summary>
-        public bool Stop()
-        {
-            // Notify other modules we're stopping
-            this.Stopping?.Invoke(this, EventArgs.Empty);
-
-            // Dispose and get rid of the timer
-            this.m_helloTimer?.Dispose();
-            this.m_helloTimer = null;
-
-            this.Stopped?.Invoke(this, EventArgs.Empty);
-            return true;
-        }
+        // Notify of start
+        this.Started?.Invoke(this, EventArgs.Empty);
+        return true;
     }
+
+    /// <summary>
+    /// Stop the hello daemon
+    /// </summary>
+    public bool Stop()
+    {
+        // Notify other modules we're stopping
+        this.Stopping?.Invoke(this, EventArgs.Empty);
+
+        // Dispose and get rid of the timer
+        this.m_helloTimer?.Dispose();
+        this.m_helloTimer = null;
+
+        this.Stopped?.Invoke(this, EventArgs.Empty);
+        return true;
+    }
+}
 }
